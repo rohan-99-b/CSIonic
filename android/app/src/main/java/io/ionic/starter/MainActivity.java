f6a8104f486bdeacafe5f4f7d87da216f44cc893
Rohan Bera <67726699+rohan-99-b@users.noreply.github.com>
2021-10-27 09:30:18 +0530

    contact_plugin_NW

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,22 @@
 package io.ionic.starter;
+// import ch.byrds.capacitor.contacts.Contacts;
+
+// import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
+// import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+// import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+    // @Override
+    // public void onCreate(Bundle saveInstanceState){
+    //     super.onCreate(saveInstanceState);
+
+    //     this.init(saveInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+    //         // Additional plugins you've installed go here
+    //         // Ex: add(TotallyAwesomePlugin.class);
+    //          add(Contacts.class);
+    //     }}); 
+    // }
+}
--- src/app/contacts/contacts.page.ts
+++ src/app/contacts/contacts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contacts} from '@capacitor-community/contacts'
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPage implements OnInit {
 
+  contact:any;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  //  loadContact(){
+  //   Contacts.getContacts().then(result => {
+  //     console.log(result);
+  //     this.contact=result.contacts
+  //     // for (const contact of result.contacts) {
+  //     //     console.log(contact);
+  //     // }
+  // });
+  // }
+ 
 
 }
